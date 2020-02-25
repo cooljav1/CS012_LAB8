72b6b6289e09d69995241e3b98526b3f09d2efe8
cooljav1 <eddiesegura596@gmail.com>
2020-02-25 14:06:00 -0800

    from laptop

--- cmake-build-debug/CMakeFiles/LAB8.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/LAB8.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "D:/projects/LAB8/main.cpp" "D:/projects/LAB8/cmake-build-debug/CMakeFiles/LAB8.dir/main.cpp.obj"
+  "C:/Users/eddie/CLionProjects/CS012_LAB8/main.cpp" "C:/Users/eddie/CLionProjects/CS012_LAB8/cmake-build-debug/CMakeFiles/LAB8.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
